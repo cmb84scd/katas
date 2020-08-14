24b17940d1cf1ca572704a102efda8962b414aa1
Catriona <catrionamairi@blueyonder.co.uk>
2020-08-14 16:14:03 +0100

    Add completed class conundrum kata

---
+++ class_conundrum/lib/class_conundrum.rb
@@ -0,0 +1,16 @@
+class List
+  attr_reader :items, :counter
+  def initialize(type)
+    @type = type
+    @items = []
+    @counter = 0
+    self
+  end
+
+  def add(item)
+    if item.class != @type then return "This item is not of type: #{@type}" end
+    @items << item
+    @counter += 1
+    self
+  end
+end
---
+++ class_conundrum/spec/class_conundrum_spec.rb
@@ -0,0 +1,20 @@
+require 'class_conundrum'
+
+describe List do
+  let(:my_list) { List.new(String) }
+
+  describe '#add' do
+    it 'returns a count of 1' do
+      expect(my_list.add('Hello').counter).to eq 1
+    end
+
+    it 'returns a count of 3' do
+      my_list.add('Hello')
+      expect(my_list.add(' ').add('World!').counter).to eq 3
+    end
+
+    it 'checks if item is of type' do
+      expect(my_list.add(5)).to eq "This item is not of type: String"
+    end
+  end
+end
