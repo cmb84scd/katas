17a30e94098b1dbda27687dfa8630405fff9af95
cmb84scd <cmb84scd@gmail.com>
2021-03-12 17:21:26 +0000

    Add refactored create phone number

--- create_phone_number/phone_no.py
+++ create_phone_number/phone_no.py
@@ -1,5 +1,8 @@
+def array_to_string(a):
+  return ''.join(str(e) for e in a)
+
 def create_phone_number(n):
-  str1 = ''.join(str(e) for e in n[:3])
-  str2 = ''.join(str(e) for e in n[3:6])
-  str3 = ''.join(str(e) for e in n[6:])
+  str1 = array_to_string(n[:3])
+  str2 = array_to_string(n[3:6])
+  str3 = array_to_string(n[6:])
   return f"({str1}) {str2}-{str3}"
