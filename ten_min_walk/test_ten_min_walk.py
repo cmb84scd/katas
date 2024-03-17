a1d92424bc507504c3f30f5de6b00d45a1a60392
Catriona Bennett <catrionamairi@blueyonder.co.uk>
2024-03-17 16:29:48 +0000

    Use pytest in ten_min_walk

--- ten_min_walk/test_ten_min_walk.py
+++ ten_min_walk/test_ten_min_walk.py
@@ -1,16 +1,15 @@
-import unittest
 from ten_min_walk import is_valid_walk
 
-class TestIsValidWalk(unittest.TestCase):
+class TestIsValidWalk:
 
   def test_walk_not_long_enough(self):
-    self.assertFalse(is_valid_walk(['w', 's']))
+    assert is_valid_walk(['w', 's']) is False
 
   def test_valid_walk(self):
-    self.assertTrue(is_valid_walk(['w', 's', 'e', 'e', 'n', 'n', 'e', 's', 'w', 'w']))
+    assert is_valid_walk(['w', 's', 'e', 'e', 'n', 'n', 'e', 's', 'w', 'w']) is True
 
   def test_walk_is_too_long(self):
-    self.assertFalse(is_valid_walk(['w', 'e', 'w', 'e', 'w', 'e', 'w', 'e', 'w', 'e', 'w', 'e']))
+    assert is_valid_walk(['w', 'e', 'w', 'e', 'w', 'e', 'w', 'e', 'w', 'e', 'w', 'e']) is False
 
   def test_walk_is_invalid(self):
-    self.assertFalse(is_valid_walk(['e', 's', 'e', 'e', 'n', 'n', 'e', 's', 'w', 'w']))
+    assert is_valid_walk(['e', 's', 'e', 'e', 'n', 'n', 'e', 's', 'w', 'w']) is False
