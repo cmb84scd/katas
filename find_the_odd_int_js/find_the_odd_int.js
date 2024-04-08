cabd361c3f2f8ccb9f4f82416772d79368b1fe1c
Catriona Bennett <catrionamairi@blueyonder.co.uk>
2024-04-08 18:00:58 +0100

    Add refactored find the odd int js

--- find_the_odd_int_js/find_the_odd_int.js
+++ find_the_odd_int_js/find_the_odd_int.js
@@ -1,9 +1,4 @@
 function findOdd(arr) {
-  for (let i in arr) {
-    const occ = arr.filter((v) => (v === arr[i])).length
-    if (occ % 2 !== 0) {
-      return arr[i]
-    }
-  }
+  return arr.find((num) => arr.filter((n) => num === n).length % 2)
 }
 module.exports = findOdd;
