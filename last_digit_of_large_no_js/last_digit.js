704a011b09ae991b5d65178b3cfa89d82def5e17
Catriona Bennett <catrionamairi@blueyonder.co.uk>
2024-04-11 16:12:22 +0100

    Add refactor of last digit of large no is js

--- last_digit_of_large_no_js/last_digit.js
+++ last_digit_of_large_no_js/last_digit.js
@@ -1,10 +1,8 @@
 function lastDigit(n, m) {
-  if (m === 0n) {
-    return 1n
-  } else if (n % 10n === 0n & m % 10n === 0n) {
-    return 0n
-  } else {
-    return ((n % 100n) ** (m % 40n)) % 10n
-  }
+  if (m === 0n) return 1n
+
+  if (n % 10n === 0n & m % 10n === 0n) return 0n
+
+  return ((n % 100n) ** (m % 40n)) % 10n
 }
 module.exports = lastDigit;
