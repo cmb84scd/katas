3dbc49ad16e6ebaf509352b958d3f123702e1816
Catriona Bennett <catrionamairi@blueyonder.co.uk>
2025-02-06 12:42:29 +0000

    Add alternative to unusual sorting in js

--- javascript/unusual_sort/solution.js
+++ javascript/unusual_sort/solution.js
@@ -28,3 +28,10 @@ function unusualSort(arrayOfChars) {
   return strList.concat(numList)
 }
 module.exports = unusualSort;
+
+
+// The above is my version and the below is a one line alternative
+// function unusualSort(arrayOfChars) {
+//   const sorted = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',0,'0',1,'1',2,'2',3,'3',4,'4',5,'5',6,'6',7,'7',8,'8',9,'9'];
+//   return arrayOfChars.sort((a,b)=>sorted.indexOf(a) - sorted.indexOf(b));
+// }
