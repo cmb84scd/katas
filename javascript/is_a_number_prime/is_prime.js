9ac983824b4524f78842d6b176fbcfa13a1c9f9b
Catriona Bennett <catrionamairi@blueyonder.co.uk>
2025-02-14 14:52:33 +0000

    Refactor and add note for is a num prime in js

--- javascript/is_a_number_prime/is_prime.js
+++ javascript/is_a_number_prime/is_prime.js
@@ -1,7 +1,11 @@
 function isPrime(num) {
   if (num <= 1) return false
+  /* Calculating sqrt here means only doing it once,
+  which more efficient as the loop will do it each time.
+  */
+  const limit = Math.sqrt(num)
 
-  for (let i = 2; i <= Math.sqrt(num); i++) {
+  for (let i = 2; i <= limit; i++) {
     if (num % i === 0) return false
   }
 
