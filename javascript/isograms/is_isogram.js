786540ba95f78dcc1c0ab1ad13fcae8eab86ce59
Catriona Bennett <catrionamairi@blueyonder.co.uk>
2025-03-05 15:49:44 +0000

    Add refactored isograms in js

--- javascript/isograms/is_isogram.js
+++ javascript/isograms/is_isogram.js
@@ -1,8 +1,14 @@
 function isIsogram(str) {
-  if (str.length === new Set(str.toLowerCase()).size) {
-    return true
-  }
-
-  return false
+  return str.length === new Set(str.toLowerCase()).size
 }
+
+// Below is my original answer and above is my rafactored one
+
+// function isIsogram(str) {
+//   if (str.length === new Set(str.toLowerCase()).size) {
+//     return true
+//   }
+
+//   return false
+// }
 module.exports = isIsogram;
