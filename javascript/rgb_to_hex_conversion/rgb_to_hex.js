7c7d865e6a4dbc8fe1e0c77432218b567def64d8
Catriona Bennett <catrionamairi@blueyonder.co.uk>
2025-03-20 16:45:02 +0000

    Add refactored rgb to hex conversion in js

--- javascript/rgb_to_hex_conversion/rgb_to_hex.js
+++ javascript/rgb_to_hex_conversion/rgb_to_hex.js
@@ -1,8 +1,16 @@
 function rgb(r, g, b) {
-  return [r, g, b].map((x) => {
-    if (x > 255) return 'FF'
-    if (x < 0) return '00'
-    return x.toString(16).toUpperCase().padStart(2, '0')
-  }).join('')
+  return [r, g, b].map((x) => Math.max(0, Math.min(255, x))
+    .toString(16).toUpperCase().padStart(2, '0')
+  ).join('')
 }
+
+// Below is my original answer and above is my refactored one
+
+// function rgb(r, g, b) {
+//   return [r, g, b].map((x) => {
+//     if (x > 255) return 'FF'
+//     if (x < 0) return '00'
+//     return x.toString(16).toUpperCase().padStart(2, '0')
+//   }).join('')
+// }
 module.exports = rgb;
