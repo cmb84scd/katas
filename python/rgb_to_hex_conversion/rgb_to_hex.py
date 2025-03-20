fd9a2b0cacda8c35df3b3fb2d511628e09ca2e02
Catriona Bennett <catrionamairi@blueyonder.co.uk>
2025-03-20 16:19:11 +0000

    Add refactored rgb to hex conversion in py

--- python/rgb_to_hex_conversion/rgb_to_hex.py
+++ python/rgb_to_hex_conversion/rgb_to_hex.py
@@ -1,7 +1,13 @@
 def rgb(r, g, b):
-    rgb_arr = [r, g, b]
-    rgb_arr = [0 if x < 0 else 255 if x > 255 else x for x in rgb_arr]
+    return "".join([f"{max(0, min(x, 255)):02X}" for x in [r, g, b]])
 
-    r, g, b = rgb_arr
 
-    return f"{r:02X}{g:02X}{b:02X}"
+# Below is my orignal answer and above is my refactored one
+
+# def rgb(r, g, b):
+#     rgb_arr = [r, g, b]
+#     rgb_arr = [0 if x < 0 else 255 if x > 255 else x for x in rgb_arr]
+
+#     r, g, b = rgb_arr
+
+#     return f"{r:02X}{g:02X}{b:02X}"
